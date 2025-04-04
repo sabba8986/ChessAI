a68274d2c236b8e14441c02aef42da48793b71f0
sabba8986 <sabba.8986@gmail.com>
2025-04-04 17:26:02 -0400

    Added documentation for BasicPiece, defines

--- BasicPiece.hpp
+++ BasicPiece.hpp
@@ -1,3 +1,11 @@
+/*
+ * Definition for a BasicPiece struct. A BasicPiece struct contains two simple attributes:
+ * Position of the piece 
+ * Color of the piece 
+ *
+ * It does NOT store information about the piece type.
+ */
+
 #ifndef BASIC_PIECE
 #define BASIC_PIECE
 
